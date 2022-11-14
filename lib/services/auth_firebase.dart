5945881ea9c833852b78e57f5c0d8e0ec1607464
daniel-demelash <danieldemelash5@gmail.com>
2022-11-14 13:45:57 +0300

    rename: user model class

--- lib/services/auth_firebase.dart
+++ lib/services/auth_firebase.dart
@@ -6,7 +6,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
-import '../models/user.dart' as model;
+import '../models/user_model.dart' as model;
 
 class Auth {
   final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
